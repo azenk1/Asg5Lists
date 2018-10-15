16cfc3eff3f84fb5f0d67a6a7839bb66b8c194de
Alex Zenk <azenk@css.edu>
2018-10-14 20:50:10 -0500

    Links in progress

---
+++ src/exceptions/EmptyCollectionException.java
@@ -0,0 +1,23 @@
+package jsjf.exceptions;
+
+/**
+ * Represents the situation in which a collection is empty.
+ *
+ * @author Lewis and Chase
+ * @version 4.0
+ */
+public class EmptyCollectionException extends RuntimeException
+{
+    /**
+     * Sets up this exception with an appropriate message.
+     * @param collection the name of the collection
+     */
+    public EmptyCollectionException (String collection)
+    {
+        super ("The " + collection + " is empty.");
+    }
+
+    public EmptyCollectionException() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
